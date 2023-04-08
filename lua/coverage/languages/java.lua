c9ef0bb79d3d62a9816743c0d1a6b2a605f06fe3
Leandro Conca <lluccia@gmail.com>
2023-04-08 14:24:56 -0300

    coverage summary per file and fixes module folder

---
+++ lua/coverage/languages/java.lua
@@ -0,0 +1,164 @@
+local M = {}
+
+local signs = require("coverage.signs")
+local xmlreader = require("xmlreader")
+
+local files_line_coverage = function(xmlfile)
+    local reader = xmlreader.from_file(xmlfile)
+
+    if not reader then
+        print("xml file was not loaded")
+        return
+    end
+
+    local files_line_coverage = {}
+
+    local package_name
+    local sourcefile_name
+    local coveredlines = {}
+    local uncoveredlines = {}
+
+    while reader:read() do
+        if reader:local_name() == "package" and reader:node_type() == "element" then
+            package_name = reader:get_attribute("name")
+        end
+
+        if reader:local_name() == "sourcefile" and reader:node_type() == "element" then
+            sourcefile_name = reader:get_attribute("name")
+        end
+
+        if reader:local_name() == "line" and reader:node_type() == "element" then
+            local line_number = reader:get_attribute("nr")
+            local covered = reader:get_attribute("cb") ~= "0" or reader:get_attribute("ci") ~= "0"
+
+            if covered then
+                table.insert(coveredlines, line_number)
+            else
+                table.insert(uncoveredlines, line_number)
+            end
+        end
+
+        if reader:local_name() == "sourcefile" and reader:node_type() == "end element" then
+            local source_data= {
+                source_path = package_name .. "/" .. sourcefile_name,
+                coveredlines = coveredlines,
+                uncoveredlines = uncoveredlines
+            }
+            table.insert(files_line_coverage, source_data)
+            coveredlines = {}
+            uncoveredlines = {}
+        end
+    end
+    return files_line_coverage
+end
+
+local files_summary = function(xmlfile)
+    local reader = xmlreader.from_file(xmlfile)
+
+    if not reader then
+        print("xml file was not loaded")
+        return
+    end
+
+    local files_summary = {}
+    local file_summary = {}
+    local class_name
+    local counters = {}
+
+    while reader:read() do
+        if reader:local_name() == "class" and reader:node_type() == "element" then
+            class_name = reader:get_attribute("name")
+        end
+
+        if reader:local_name() == "counter" and reader:node_type() == "element" then
+            local counter_type = reader:get_attribute("type")
+            local counter_missed = tonumber(reader:get_attribute("missed"))
+            local counter_covered = tonumber(reader:get_attribute("covered"))
+
+            counters[counter_type] = {
+                missed = counter_missed,
+                covered = counter_covered,
+                total = counter_missed + counter_covered
+            }
+        end
+
+        if reader:local_name() == "class" and reader:node_type() == "end element" then
+            file_summary.filename = class_name
+            file_summary.statements = counters['INSTRUCTION'].total
+            file_summary.missing = counters['INSTRUCTION'].missed
+            if counters['BRANCH'] then
+                file_summary.branches = counters['BRANCH'].total
+                file_summary.partial = counters['BRANCH'].missed
+            else
+                file_summary.branches = 0
+                file_summary.partial = 0
+            end
+
+            file_summary.coverage =  math.floor(
+                counters['INSTRUCTION'].covered / counters['INSTRUCTION'].total * 100)
+
+            table.insert(files_summary, file_summary)
+
+            file_summary = {}
+            class_name = nil
+            counters = {}
+        end
+    end
+
+    return files_summary
+end
+
+--- Loads a coverage report.
+-- This method should perform whatever steps are necessary to generate a coverage report.
+-- The coverage report results should passed to the callback, which will be cached by the plugin.
+-- @param callback called with results of the coverage report
+M.load = function(callback)
+    local xmlfile = "tests/jacoco.xml"
+
+    local data = {}
+    data.line_coverage = files_line_coverage(xmlfile)
+    data.files_summary = files_summary(xmlfile)
+
+    callback(data)
+end
+
+
+
+--- Returns a list of signs that will be placed in buffers.
+-- This method should use the coverage data (previously generated via the load method) to 
+-- return a list of signs.
+-- @return list of signs
+M.sign_list = function(data)
+    local sign_list = {}
+    for _, source_data in pairs(data.line_coverage) do
+        local buffer = vim.fn.bufnr( "src/main/java/" .. source_data.source_path, false)
+
+        if buffer ~= -1 then
+            for _, line in ipairs(source_data.coveredlines) do
+                table.insert(sign_list, signs.new_covered(buffer, line))
+            end
+
+            for _, line in ipairs(source_data.uncoveredlines) do
+                table.insert(sign_list, signs.new_uncovered(buffer, line))
+            end
+        end
+    end
+    return sign_list
+end
+
+--- Returns a summary report.
+-- @return summary report
+M.summary = function(data)
+    return {
+        files = data.files_summary,
+        totals = {
+            statements = data.total_statements,     -- number of total statements in the report
+            missing = data.total_missing,           -- number of lines missing coverage (uncovered) in the report
+            branches = data.total_branches,         -- number of total branches in the report
+            partial = data.total_partial_branches,  -- number of branches that are partially covered in the report
+            coverage = data.total_coverage,         -- coverage percentage to display in the report
+        }
+    }
+end
+
+return M
--- tests/java_spec.lua
+++ tests/java_spec.lua
@@ -6,16 +6,16 @@ describe("coverage-java", function()
 
     it("can get covered and uncovered lines", function()
         local coverage_java = require("coverage.languages.java")
-        local actual_data
-        local callback = function(data)
-            actual_data = data
-        end
-        coverage_java.load(callback)
 
-        assert.equal(6, #actual_data)
+        local actual_data
+        coverage_java.load(function(data)
+            actual_data = data
+        end)
+
+        assert.equal(6, #actual_data.line_coverage)
         local files = {}
 
-        for _, file in pairs(actual_data) do
+        for _, file in pairs(actual_data.line_coverage) do
             table.insert(files, file.source_path)
         end
         assert.same({
@@ -27,7 +27,7 @@ describe("coverage-java", function()
              'dev/conca/mavenconversion/mavencentral/MavenCentralSearch.java',
         }, files)
 
-        local first_file = actual_data[1]
+        local first_file = actual_data.line_coverage[1]
 
         assert.equal("dev/conca/mavenconversion/model/Artifact.java", first_file.source_path)
 
@@ -38,4 +38,69 @@ describe("coverage-java", function()
             "97", "98", "99", "100", "104" }, first_file.coveredlines)
     end)
 
+    it("can get coverage data per file", function()
+        local coverage_java = require("coverage.languages.java")
+
+        local actual_data
+        coverage_java.load(function(data)
+            actual_data = data
+        end)
+
+        assert.equal(7, #actual_data.files_summary)
+
+        local first_file = actual_data.files_summary[1]
+
+        assert.equals("dev/conca/mavenconversion/model/Artifact", first_file.filename)
+        assert.equals(107, first_file.statements)
+        assert.equals(44, first_file.missing)
+        assert.equals(0, first_file.branches)
+        assert.equals(0, first_file.partial)
+        assert.equals(58, first_file.coverage)
+
+    end)
+
+    it("can report summary data", function()
+        local coverage_java = require("coverage.languages.java")
+
+        local files_summary = {}
+        local file_summary = {
+            filename = 'dev/conca/mavenconversion/model/Artifact',
+            statements = 107,
+            missing = 44,
+            branches = 0,
+            partial = 0,
+            coverage = 58,
+        }
+
+        table.insert(files_summary, file_summary)
+
+        local data = {
+            files_summary = files_summary,
+            total_statements = 517,
+            total_missing = 417,
+            total_branches = 18,
+            total_partial_branches = 14,
+            total_coverage = 22,
+        }
+
+        local summary = coverage_java.summary(data)
+
+        local first_file = summary.files[1]
+        assert.same({
+            filename = "dev/conca/mavenconversion/model/Artifact",
+            statements = 107,
+            missing = 44,
+            branches = 0,
+            partial = 0,
+            coverage = 58,
+        }, first_file)
+
+        assert.same({
+            statements = 517,
+            missing = 417,
+            branches = 18,
+            partial = 14,
+            coverage = 22,
+        }, summary.totals)
+    end)
 end)
